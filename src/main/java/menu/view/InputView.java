d366785f6527e85bcd0cb4d74870d7d3ddbd6d19
JaeY0ungJung <hanol5216@gmail.com>
2023-12-13 15:45:42 +0900

    feat: feat the function that get coaches' dislike foods

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,8 +6,12 @@ public class InputView {
 
     public String getCoachNames() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
-        String input = Console.readLine();
-        return input;
+        return Console.readLine();
+    }
+
+    public String getCoachDislikeFoods(String coachName) {
+        System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.\n", coachName);
+        return Console.readLine();
     }
 
 }
